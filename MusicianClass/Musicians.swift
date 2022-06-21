9d06f5c774aef0c850549a2b356d2badbdb6b60d
kadirakyol <kadir.akyol.ce@gmail.com>
2022-06-21 12:42:12 +0300

    first commit test

---
+++ MusicianClass/Musicians.swift
@@ -0,0 +1,16 @@
+//
+//  Musicians.swift
+//  MusicianClass
+//
+//  Created by Kadir Akyol on 21.06.2022.
+//
+
+import Foundation
+
+class Musicians{
+    // Property
+    var name = ""
+    var age = 0
+    var instrument = ""
+    
+}
